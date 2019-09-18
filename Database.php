b911f2c1c99d890cc990cd3c4cdd5d41d45f49f4
Muhammad Angga Kawa Perdana <kawam@hasnurgroup.local>
2019-09-18 13:17:27 +0800

    first commit

---
+++ Database.php
@@ -0,0 +1,55 @@
+<?php
+
+class Database{
+    // db settings
+    private $host   = 'localhost';
+    private $user   = 'root';
+    private $dbname = 'excel-db';
+    private $pass   = '';
+
+    private $dbh;
+    private $error;
+
+    public function __construct() {
+        $dsn = 'mysql: host=' . $this->host . ';dbname=' . $this->dbname;
+        $options = array(
+            PDO::ATTR_PERSISTENT            => true,
+            PDO::ATTR_ERRMODE               => PDO::ERRMODE_EXCEPTION,
+            PDO::MYSQL_ATTR_INIT_COMMAND    => 'SET NAMES UTF8'
+        );
+        try{
+            $this->dbh = new PDO($dsn, $this->user, $this->pass, $options); 
+        }
+        catch(PDOException $e){
+            $this->error = $e->getMessage();
+            echo $this->error;
+            exit;
+        }       
+    }
+    
+    public function createTable() {
+      // create table based on what you need
+      $query = "CREATE TABLE IF NOT EXISTS hasil (
+          id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+          name VARCHAR(60) NOT NULL
+        )";
+      $this->dbh->exec($query);
+      echo "Table created successfully" . PHP_EOL;
+    }
+
+    // change the argument based on what you need
+    public function insertToTable($rows) {
+      // check the $rows structure
+      // var_dump($rows);
+
+      $value_1 = $rows["A"];
+      $value_2 = $rows["B"];
+
+      $stmt = $this->dbh->prepare("INSERT INTO hasil (id, name) VALUES (:value_1, :value_2)");
+      $stmt->bindParam(':value_1', $value_1);
+      $stmt->bindParam(':value_2', $value_2);
+      $stmt->execute();
+
+    }
+
+}
---
+++ README.md
@@ -0,0 +1,20 @@
+# Spreadsheet Scan
+
+*Still in Development*
+
+> I'm using [Phpoffice/PhpSpreadsheet](https://github.com/PHPOffice/PhpSpreadsheet) library, check it out !!!
+
+A tool to exporting excel data to mysql database. Based on PHP 7.
+
+## How to Use
+First, change the Database.php based on what you need.
+
+Command line version : 
+```sh
+$ composer install
+$ php spreadsheet-scan.php "excelfile.xls | spreadsheet.csv" "rangeofcell"
+```
+example: 
+```sh
+$ php spreadsheet-scan.php "test.xlsx" "a2:b6"
+```
